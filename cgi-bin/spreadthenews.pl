f3c5d5140a4da26e503a1359e1186ee7180c91ae
Simon Huggins <huggie@earth.li>
2014-01-17 09:50:07 +0000

    Return the data

--- cgi-bin/spreadthenews.pl
+++ cgi-bin/spreadthenews.pl
@@ -6,11 +6,22 @@ use warnings;
 use CGI;
 
 my $q = CGI->new;
+my $file = '/home/huggie/tmp/spreadthenews.log';
+
+
+sub error($) {
+	my $err = shift;
+	print "Content-Type: text/html\n\n";
+	
+	print "<h1>Error</h1>\n";
+	print "<p>" . $err . "</p>\n";
+	exit;
+}
 
 if ($q->param('data')) {
 	# deal with POST
-	open(OUT, ">/home/huggie/tmp/spreadthenews.log");
-	print OUT $p->param('data');
+	open(OUT, ">$file");
+	print OUT $q->param('data');
 	close(OUT);
 
 	print "Content-Type: text/html\n";
@@ -18,6 +29,24 @@ if ($q->param('data')) {
 
 	print "Thanks!\n";
 	exit;
+} elsif ($q->param('getlist')) {
+	my $user =  $q->param('getlist');
+	if ($user ne 'shufgy') {
+		error("No user known by that name!\n");	
+	}
+
+	my $data;
+	open(IN, $file);
+	{
+		undef $/;
+		$data = <IN>;
+	}
+	close(IN);
+
+	print "Content-Type: application/json\n\n";
+	print $data;
+} else {
+	print "Content-Type: text/html\n\n";
+	print "<h1>Tsk</h1>\n";
+	print "<p>Nothing here for the curious</p>\n";
 }
-
-
