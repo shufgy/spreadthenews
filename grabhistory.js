fd1dd91be28f7474c1ace8703e0272e2bd374f9d
Simon Huggins <huggie@earth.li>
2014-01-17 09:19:43 +0000

    Working POST

--- grabhistory.js
+++ grabhistory.js
@@ -2,3 +2,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.method == "getGuHistory")
       sendResponse({history: JSON.parse(localStorage['gu.history'])});
 });
+
+//document.addEventListener('DOMContentLoaded', function () {
+//  require(['common/modules/identity/api'], function(api){
+//    // block has api in scope
+//    console.log(api.getUserFromApi())
+//  });
+//});
--- popup.js
+++ popup.js
@@ -78,23 +78,35 @@ var kittenGenerator = {
 };
 
 var histwin = {
-  putItem: function(key, value) {
-    window.localStorage.removeItem(key);
-    window.localStorage.setItem(key, value);
+  resetList: function() {
+    window.localStorage.removeItem('list');
+  },
+  getList: function() {
+    list = window.localStorage.getItem('list');
+    if (!list) return [];
+    return JSON.parse(list);
   },
   show: function() {
-    var blah = document.createElement('p');
-    blah.innerHTML = window.localStorage.getItem('mything')
-    document.body.appendChild(blah);
+    listHTML = document.getElementById('list');
+    listHTML.innerHTML = "<p>Here's your list!</p>";
+    list = histwin.getList();
+    for (index = 0; index < list.length; ++index) {
+      item = list[index];
+      var blah = document.createElement('p');
+      blah.innerHTML = item;
+      listHTML.appendChild(blah);
+    }
   },
   append: function(value) {
-    histwin.putItem("mything", value);
-    histwin.show();
+    list = histwin.getList();
+    list = list.concat(value);
+    window.localStorage.setItem('list', JSON.stringify(list));
   },
   store: function() {
     var blah = document.getElementById('storeme');
     if (blah.value == "") { return true; }
     histwin.append(blah.value)
+    histwin.show();
     blah.value = "";
   },
   grabhist: function() {
@@ -103,15 +115,28 @@ var histwin = {
       r = response
       console.log(r);
       if (r && r.history && r.history.value) {
+        histwin.resetList()
         for (index = 0; index < r.history.value.length; ++index) {
           item = r.history.value[index];
           url = 'http://www.theguardian.com/' + item.id;
           histwin.append(url)
         }
-        //histwin.append(r.history);
+        histwin.show();
       }
       });
     });
+  },
+  submitList: function() {
+    list = histwin.getList();
+    if (list.length == 0) {
+      alert("Nothing to submit!");
+      return;
+    }
+    $.post("http://the.earth.li/~huggie/cgi-bin/spreadthenews.pl",
+      { data: JSON.stringify(list) })
+      .done(function( data ) {
+        alert( "Data Loaded: " + data );
+      });
   },
 };
 
@@ -120,4 +145,6 @@ document.addEventListener('DOMContentLoaded', function () {
     text.addEventListener('change', histwin.store);
     var hist = document.getElementById('grabHistory');
     hist.addEventListener('click', histwin.grabhist);
+    var send = document.getElementById('submitList');
+    send.addEventListener('click', histwin.submitList);
 });
